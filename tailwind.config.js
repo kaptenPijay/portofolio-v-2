b00e3477d422004b60a643fdd46617f10a9ac70f
kaptenPijay <muhammadzahran0205@gmail.com>
2024-12-28 21:14:59 +0700

    add : add about

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,6 +10,10 @@ module.exports = {
       colors: {
         primary: "#bd00ff",
         dark: "#0f172a",
+        secondary: "#64748b",
+        boxShadow: {
+          primary: "0 10px 20px #bd00ff",
+        },
       },
       screens: {
         "2xl": "1320px",
