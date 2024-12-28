ce4563bb0c064d0d1f7c0875e9de17d983722a9b
kaptenPijay <muhammadzahran0205@gmail.com>
2024-12-29 05:43:11 +0700

    change image

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,11 +8,11 @@ module.exports = {
     },
     extend: {
       colors: {
-        primary: "#bd00ff",
+        primary: "#8f00ff",
         dark: "#0f172a",
         secondary: "#64748b",
         boxShadow: {
-          primary: "0 10px 20px #bd00ff",
+          primary: "0 10px 20px #8f00ff",
         },
       },
       screens: {
